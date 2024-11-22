268d594cd961c3aaef40fe7d2fba021fefb561f8
Lenny Gonzalez <98600094+Morefew@users.noreply.github.com>
2024-11-22 10:32:16 -0400

    feat: implemented api connection API connection service implemented to handle any API needed.

---
+++ src/main/java/com/myco/literalura/service/ConnectionToAPI.java
@@ -0,0 +1,36 @@
+package com.myco.literalura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ * project literalura
+ *
+ *
+ * @author Lenny Gonzalez
+ * 12:40 PM 11/19/2024 2024
+ * @Version 1.0.0.0
+ * @since 11/19/2024
+ */
+public class ConnectionToAPI {
+    public String fetchData(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String json = response.body();
+        return json;
+    }
+}
