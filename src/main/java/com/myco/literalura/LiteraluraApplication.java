2ad07f911d30f04c1f9d57ba3d73c73ea5b8cb6a
Lenny Gonzalez <98600094+Morefew@users.noreply.github.com>
2024-11-22 10:37:22 -0400

    feat: App entry point

---
+++ src/main/java/com/myco/literalura/LiteraluraApplication.java
@@ -0,0 +1,26 @@
+package com.myco.literalura;
+
+import com.myco.literalura.repository.BookRepository;
+import com.myco.literalura.view.LiteraluraApp;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+    private BookRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        LiteraluraApp literaluraApp = new LiteraluraApp(repository);
+        literaluraApp.run();
+
+    }
+}
