0225ec81025d4453b80a1041466952ca787a6d3a
Lenny Gonzalez <98600094+Morefew@users.noreply.github.com>
2024-11-25 14:40:20 -0400

    refactor: redifined mapper implementation

--- src/main/java/com/myco/literalura/service/DtoMapper.java
+++ src/main/java/com/myco/literalura/service/DtoMapper.java
@@ -1,22 +1,33 @@
 package com.myco.literalura.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
  * project literalura
  *
- * @author User
+ * @author Lenny Gonzalez
  * 12:46 PM 11/19/2024 2024
- * @Version 1.0.0.0
+ * @version 1.0.0.0
  * @since 11/19/2024
  */
 public class DtoMapper implements IDtoMapper {
-private ObjectMapper objectMapper =new ObjectMapper();
+
+    private ObjectMapper objectMapper =new ObjectMapper();
+
     @Override
-    public <T> T getObjectData(String json, Class<T> clase) {
+    public <T> T mapToDto(String json, Class<T> clase) {
         try {
             return objectMapper.readValue(json,clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JsonNode mapToJsonNode(String json) {
+        try {
+            return objectMapper.readValue(json, JsonNode.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
